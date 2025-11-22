e8b6fc98d744265af39ae7581acc497eac409a6f
Farrux Temurov <tfarrux70@gmail.com>
2025-11-22 21:36:48 +0100

    update useReducer

--- frontend/src/reducers/todoReducer.js
+++ frontend/src/reducers/todoReducer.js
@@ -1,19 +1,20 @@
 export const todoReducer = (state, action) => {
     switch (action.type) {
+
+        case "SET_TODOS":
+            return action.payload;
+
         case "ADD_TODO":
-            return [...state, { id: Date.now(), text: action.payload, done: false }];
-        case "TOGGLE_TODO":
+            return [action.payload, ...state];
+
+        case "UPDATE_TODO":
             return state.map(todo =>
-                todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+                todo._id === action.payload._id ? action.payload : todo
             );
+
         case "DELETE_TODO":
-            return state.filter(todo => todo.id !== action.payload);
-        case "EDIT_TODO":
-            return state.map(todo =>
-                todo.id === action.payload.id
-                    ? { ...todo, text: action.payload.text }
-                    : todo
-            );
+            return state.filter(todo => todo._id !== action.payload);
+
         default:
             return state;
     }

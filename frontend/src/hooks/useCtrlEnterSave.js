755f22e2a7fbd1608b6710db615a8c7f836aed89
Farrux Temurov <tfarrux70@gmail.com>
2025-12-09 20:14:33 +0100

    change Enter key handling

--- frontend/src/hooks/useCtrlEnterSave.js
+++ frontend/src/hooks/useCtrlEnterSave.js
@@ -1,10 +1,11 @@
 import { useCallback } from "react";
 
-export default function useCtrlEnterSave(ref, callback, value, cancelCallback) {
+export default function useCtrlEnterSave(ref, callback, cancelCallback) {
     const handleKeyDown = useCallback((e) => {
-        if (e.key === "Enter" && e.ctrlKey) {
+        if (e.key === "Enter") {
+            if (e.shiftKey) return; // allow newline
             e.preventDefault();
-            callback(); // save
+            callback();
             if (ref.current) ref.current.style.height = "auto";
         }
 
